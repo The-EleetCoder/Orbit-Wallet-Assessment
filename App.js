0b717430b5e83ec9bf92ac7b7d1e7987f3c59b29
The-EleetCoder <jaijain1803@gmail.com>
2024-06-30 22:35:02 +0530

    added statusbar

--- App.js
+++ App.js
@@ -8,71 +8,75 @@ import { FontAwesome } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            tabBarStyle: {},
-            tabBarActiveTintColor: "#345f66",
-          }}
-        >
-          <Tab.Screen
-            name="Home"
-            component={FeedScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons color={color} size={size} name="home" />
-              ),
-              headerShown: false,
+    <>
+      <StatusBar style="auto" />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              tabBarStyle: {},
+              tabBarActiveTintColor: "#345f66",
             }}
-          />
-          <Tab.Screen
-            name="discover"
-            component={SearchScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons color={color} size={size} name="search" />
-              ),
-              headerShown: false,
-            }}
-          />
-          <Tab.Screen
-            name="Create"
-            component={SearchScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <AntDesign name="pluscircleo" color={color} size={size} />
-              ),
-              headerShown: false,
-            }}
-          />
-          <Tab.Screen
-            name="Community"
-            component={SearchScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome5 name="users" size={size} color={color} />
-              ),
-              headerShown: false,
-            }}
-          />
-          <Tab.Screen
-            name="Me"
-            component={SearchScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome name="user-circle" size={size} color={color} />
-              ),
-              headerShown: false,
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+          >
+            <Tab.Screen
+              name="Home"
+              component={FeedScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons color={color} size={size} name="home" />
+                ),
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="discover"
+              component={SearchScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons color={color} size={size} name="search" />
+                ),
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Create"
+              component={SearchScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <AntDesign name="pluscircleo" color={color} size={size} />
+                ),
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Community"
+              component={SearchScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesome5 name="users" size={size} color={color} />
+                ),
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Me"
+              component={SearchScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesome name="user-circle" size={size} color={color} />
+                ),
+                headerShown: false,
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </>
   );
 }
